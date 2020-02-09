be39c203220471f22599531a523cdbe020e9462d
igor <igor@DESKTOP-753LCKD>
2020-02-09 11:55:23 +0200

    vc build

--- src/api/detail/path.cc
+++ src/api/detail/path.cc
@@ -113,12 +113,7 @@ namespace vfs
 	{
 		return int(_dirs.size());
 	}
-	// -----------------------------------------------------------------------------
-	std::ostream& operator<<(std::ostream& os, const path& pth)
-	{
-		os << pth.to_string();
-		return os;
-	}
+	
 
 } // namespace bsw
 
